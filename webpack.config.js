501f18e2365c12480ffc3575e350af53a4bbd8f7
wangwangwanglichao <wangwangwanglichao@foxmail.com>
2018-08-11 22:33:52 +0800

    online adapter

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+let WEBPACK_ENV = process.env.WEBPACK_ENV || "dev";
+
+console.log(WEBPACK_ENV);
+console.log(process.env.WEBPACK_ENV);
+
 module.exports = {
    entry: './src/app.jsx',
    output: {
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/dist/',
+      publicPath: WEBPACK_ENV === "dev" ? '/dist/' : "//s.jianliwu.com/admin-ve/fe/dist/",
       filename: 'js/app.js'
    },
    // 配置路径别名
